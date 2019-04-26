11ecdad62fb32fd08c2f936d31e24159b28210f9
Jonathan Schoonhoven <jtschoonhoven@gmail.com>
2019-04-26 15:30:12 -0700

    text_to_matrix

--- ledmatrix/font.py
+++ ledmatrix/font.py
@@ -5,19 +5,24 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 DEFAULT_FONT_HEIGHT_PX: int = 7
+DEFAULT_FONT_PATH = 'ledmatrix/fonts/zig.ttf'
 PIL_IMAGE_MODE_BW = '1'  # https://pillow.readthedocs.io/en/stable/handbook/concepts.html#modes
-PIL_COLOR_BLACK = 0
+PIL_COLOR_WHITE = 1
 PIL_BITMAP_ORIGIN = (0, 0)
 
 _char_cache: Dict[str, List[List[bool]]] = {}
+_text_cache: Dict[str, List[List[bool]]] = {}
 
 
 def char_to_matrix(
     char: str,
-    font_path: str = 'ledmatrix/fonts/silkscreen.ttf',
+    font_path: str = DEFAULT_FONT_PATH,
     font_height_px: int = DEFAULT_FONT_HEIGHT_PX,
+    font_expand_px: int = 0,
+    font_shift_down_px: int = 0,
 ) -> List[List[bool]]:
     """Convert a string to a 2-D matrix.
+
     From: stackoverflow.com/questions/36384353/generate-pixel-matrices-from-characters-in-string
     """
     # return from cache if available
@@ -26,24 +31,64 @@ def char_to_matrix(
         return _char_cache[char]
 
     # parse the font and write the char to a bitmap
-    font = ImageFont.truetype(font_path, font_height_px)
+    font = ImageFont.truetype(font_path, font_height_px + font_expand_px)
     bitmap_width_px, bitmap_height_px = font.getsize(char)
     bitmap_size = (bitmap_width_px, bitmap_height_px)
-    image = Image.new(mode=PIL_IMAGE_MODE_BW, size=bitmap_size, color=PIL_COLOR_BLACK)
+    image = Image.new(mode=PIL_IMAGE_MODE_BW, size=bitmap_size, color=PIL_COLOR_WHITE)
     draw = ImageDraw.Draw(im=image)
-    draw.text(PIL_BITMAP_ORIGIN, char, font=font)
+    origin = (PIL_BITMAP_ORIGIN[0], PIL_BITMAP_ORIGIN[1] + font_shift_down_px)
+    draw.text(origin, char, font=font)
 
     # populate the matrix
     matrix_rows: List[List[bool]] = []
-    for row_index in range(bitmap_height_px):
+    for row_index in range(font_height_px):
         row: List[bool] = []
         for col_index in range(bitmap_width_px):
-            if image.getpixel((col_index, row_index)):
-                row.append(True)
-            else:
+            try:
+                if image.getpixel((col_index, row_index)):
+                    row.append(False)
+                else:
+                    row.append(True)
+            except IndexError:
                 row.append(False)
+
         matrix_rows.append(row)
 
     # add to cache and return
     _char_cache[char] = matrix_rows
     return matrix_rows
+
+
+def text_to_matrix(
+    text: str,
+    font_path: str = DEFAULT_FONT_PATH,
+    font_height_px: int = DEFAULT_FONT_HEIGHT_PX,
+    font_expand_px: int = 3,
+    font_shift_down_px: int = -1,
+) -> List[List[bool]]:
+    char_matrices: List[List[List[bool]]] = []
+    text_matrix: List[List[bool]] = []
+
+    # return from cache if available
+    # NOTE: assumes we're only ever using a single font height
+    if text in _text_cache:
+        return _text_cache[text]
+
+    # transform each character in text to a 2-D pixel matrix of binary values
+    for char in text:
+        char_matrix = char_to_matrix(
+            char,
+            font_height_px=font_height_px,
+            font_expand_px=font_expand_px,
+            font_shift_down_px=font_shift_down_px,
+        )
+        char_matrices.append(char_matrix)
+
+    # join character matrices into a single matrix
+    for row_index in range(font_height_px):
+        text_matrix.append([])
+        for char_matrix in char_matrices:
+            text_matrix[row_index] = text_matrix[row_index] + char_matrix[row_index]
+
+    _text_cache[text] = text_matrix
+    return text_matrix
