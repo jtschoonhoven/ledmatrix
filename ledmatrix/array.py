137dfb2cf0b7e3c5dd5bff2611efe55833055824
Jonathan Schoonhoven <jtschoonhoven@gmail.com>
2019-04-26 13:02:28 -0700

    rename "array" as "matrix" for clarity

--- ledmatrix/array.py
+++ ledmatrix/array.py
@@ -9,19 +9,19 @@ DEFAULT_NUM_ROWS = 7
 DEFAULT_NUM_COLS = 42
 
 
-class ArrayOrigin(Enum):
+class MatrixOrigin(Enum):
     northeast: str = 'NE'
     southeast: str = 'SE'
     southwest: str = 'SW'
     northwest: str = 'NW'
 
 
-class ArrayOrientation(Enum):
+class MatrixOrientation(Enum):
     column: str = 'COL'
     row: str = 'ROW'
 
 
-class LedArray(collections.abc.Sequence):
+class LedMatrix(collections.abc.Sequence):
 
     def __init__(
         self,
@@ -31,8 +31,8 @@ class LedArray(collections.abc.Sequence):
         brightness: float = 1,
         auto_write: bool = False,
         pixel_order: color.ColorOrder = color.RGB,
-        origin: ArrayOrigin = ArrayOrigin.northeast,
-        orientation: ArrayOrientation = ArrayOrientation.column,
+        origin: MatrixOrigin = MatrixOrigin.northeast,
+        orientation: MatrixOrientation = MatrixOrientation.column,
     ) -> None:
         num_pixels = num_rows * num_cols
         gpio_pin = getattr(board, gpio_pin_name)
@@ -46,28 +46,33 @@ class LedArray(collections.abc.Sequence):
             pixel_order=pixel_order,
         )
 
-        # initialize each row in array
-        self._array: List[List[color.Color]] = []
+        # initialize each row in matrix
+        self._matrix: List[List[color.Color]] = []
         for row_index in range(num_rows):
-            self._array.append(_LedArrayRow(self, row_index, num_cols))
+            self._matrix.append(_LedMatrixRow(self, row_index, num_cols))
 
     def render(self) -> None:
         """Render current state of matrix to the neopixel (only useful when auto_write is False)."""
         self._neopixel.show()
 
     def fill(self, value: color.Color) -> None:
-        for row in self._array:
+        for row in self._matrix:
             row.fill(value)
 
-    def _neopixel_set(self, array_row_index: int, array_col_index: int, value: color.Color) -> None:
+    def _neopixel_set(
+        self,
+        matrix_row_index: int,
+        matrix_col_index: int,
+        value: color.Color,
+    ) -> None:
         """Update the NeoPixel pixel at the index corresponding to this position in the matrix."""
-        array_row = self._array[array_row_index]
-        neopixel_index = (array_row_index * len(array_row)) + array_col_index
+        matrix_row = self._matrix[matrix_row_index]
+        neopixel_index = (matrix_row_index * len(matrix_row)) + matrix_col_index
         self._neopixel[neopixel_index] = value
 
     def __repr__(self) -> str:
         buf = ''
-        for row in self._array:
+        for row in self._matrix:
             for pixel in row:
                 buf += pixel.print()
             buf += '\n'
@@ -77,22 +82,22 @@ class LedArray(collections.abc.Sequence):
         return len(self._neopixel)
 
     def __getitem__(self, index: int) -> color.Color:
-        return self._array[index]
+        return self._matrix[index]
 
     def __setitem__(self, index: int, color: color.Color) -> None:
-        return self._array[index]
+        return self._matrix[index]
 
 
-class _LedArrayRow(collections.abc.Sequence):
+class _LedMatrixRow(collections.abc.Sequence):
 
     def __init__(
         self,
-        parent_array: LedArray,
-        parent_array_index: int,
+        parent_matrix: LedMatrix,
+        parent_matrix_index: int,
         len: int,
     ) -> None:
-        self._parent_array = parent_array
-        self._parent_array_index = parent_array_index
+        self._parent_matrix = parent_matrix
+        self._parent_matrix_index = parent_matrix_index
         self._row = [color.BLACK for _ in range(len)]
 
     def fill(self, value: color.Color) -> None:
@@ -106,12 +111,12 @@ class _LedArrayRow(collections.abc.Sequence):
         return self._row[index]
 
     def __setitem__(self, index: int, value: color.Color) -> None:
-        self._row[index] = value  # update internal matrix
-        self._parent_array._neopixel_set(self._parent_array_index, index, value)  # update neopixel
+        self._row[index] = value
+        self._parent_matrix._neopixel_set(self._parent_matrix_index, index, value)
 
 
 if __name__ == '__main__':
-    a = LedArray(auto_write=False)
+    a = LedMatrix(auto_write=False)
     a.render()
     print(a)
     a[0][0] = color.RED
