9a9618abb9ebab5e5320ae41eca1ba89761b1e6a
Jonathan Schoonhoven <jtschoonhoven@gmail.com>
2019-04-28 22:23:38 -0700

    gradient

--- ledmatrix/color_cycle.py
+++ ledmatrix/color_cycle.py
@@ -10,35 +10,43 @@ class ColorCycle(matrix.LedMatrix):
 
     def __init__(self, *args, **kwargs):  # type: (*Any, **Any) -> None
         super().__init__(*args, **kwargs)
-        if not all([
-            any(value == 255 for value in self.default_color),
-            sum(value for value in self.default_color if value) == 255,
-        ]):
-            log.error('exactly one RGB must equal 255: falling back to RED')
-            self.default_color = color.RED
+
+        # fill with slight gradient
         self.fill(self.default_color)
+        for row_index in range(self.height):
+            for col_index in range(self.width):
+                pixel = self[row_index][col_index]
+                for y_offset in range(row_index):
+                    pixel = self._next_pixel_value(pixel)
+                for x_offset in range(col_index):
+                    pixel = self._next_pixel_value(pixel)
+                self[row_index][col_index] = pixel
 
     def next(self):  # type: () -> None
         for row_index in range(self.height):
             for col_index in range(self.width):
                 pixel = self[row_index][col_index]
-                red_value = pixel[0]
-                green_value = pixel[1]
-                blue_value = pixel[2]
+                next_pixel = self._next_pixel_value(pixel)
+                self[row_index][col_index] = next_pixel
 
-                if red_value == 255:
-                    green_value = green_value + 1 if green_value <= 255 else 255
-                    blue_value = blue_value - 1 if blue_value > 0 else 0
-                if green_value == 255:
-                    blue_value = blue_value + 1 if blue_value <= 255 else 255
-                    red_value = red_value - 1 if red_value > 0 else 0
-                if blue_value == 255:
-                    red_value = red_value + 1 if red_value <= 255 else 255
-                    green_value = green_value - 1 if green_value > 0 else 0
+    @staticmethod
+    def _next_pixel_value(pixel):  # type: (color.Color) -> color.Color
+        red_value = pixel[0]
+        green_value = pixel[1]
+        blue_value = pixel[2]
 
-                new_pixel = color.Color(red_value, green_value, blue_value, None)
-                self[row_index][col_index] = new_pixel
+        if red_value == 255:
+            green_value = green_value + 1 if green_value <= 255 else 255
+            blue_value = blue_value - 1 if blue_value > 0 else 0
+        elif green_value == 255:
+            blue_value = blue_value + 1 if blue_value <= 255 else 255
+            red_value = red_value - 1 if red_value > 0 else 0
+        elif blue_value == 255:
+            red_value = red_value + 1 if red_value <= 255 else 255
+            green_value = green_value - 1 if green_value > 0 else 0
 
+        next_pixel = color.Color(red_value, green_value, blue_value, None)
+        return next_pixel
 
 if __name__ == '__main__':
     import argparse
